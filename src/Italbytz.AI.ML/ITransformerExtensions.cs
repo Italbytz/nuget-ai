4aecc67d259ec239257412f1ea0093869697ab80
nunkesser <robin.nunkesser@hshl.de>
2026-05-19 22:07:26 +0200

    Update ITransformerExtensions.cs

--- src/Italbytz.AI.ML/ITransformerExtensions.cs
+++ src/Italbytz.AI.ML/ITransformerExtensions.cs
@@ -1,26 +1,43 @@
 using Microsoft.ML;
 using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Italbytz.AI.ML.Trainers;
 
 namespace Italbytz.AI.ML;
 
 public static class ITransformerExtensions
 {
-    public static IPredictionTransformer<ICanSaveModel>? ExtractIPredictionTransformer(this ITransformer transformer)
+    public static ICanSaveModel? ExtractModelParameters(this ITransformer transformer)
     {
-        if (transformer is IPredictionTransformer<ICanSaveModel> predictionTransformer)
-        {
-            return predictionTransformer;
-        }
-
         if (transformer is IEnumerable<ITransformer> chain)
         {
+            ICanSaveModel? lastExtracted = null;
             foreach (var item in chain)
             {
-                var extracted = item.ExtractIPredictionTransformer();
+                var extracted = item.ExtractModelParameters();
                 if (extracted != null)
                 {
-                    return extracted;
+                    lastExtracted = extracted;
                 }
+            }
+
+            if (lastExtracted != null)
+            {
+                return lastExtracted;
+            }
+        }
+
+        foreach (var iface in transformer.GetType().GetInterfaces())
+        {
+            if (!iface.IsGenericType || iface.GetGenericTypeDefinition() != typeof(IPredictionTransformer<>))
+            {
+                continue;
+            }
+
+            var modelProperty = iface.GetProperty("Model");
+            if (modelProperty?.GetValue(transformer) is ICanSaveModel model)
+            {
+                return model;
             }
         }
 
@@ -29,13 +46,18 @@ public static class ITransformerExtensions
 
     public static ICanSaveModel GetModelParameters(this ITransformer transformer)
     {
-        var predictionTransformer = transformer.ExtractIPredictionTransformer();
-        if (predictionTransformer == null)
+        var model = transformer.ExtractModelParameters();
+        if (model == null)
         {
             throw new InvalidOperationException(
                 "The transformer does not expose ML.NET prediction model parameters.");
         }
 
-        return predictionTransformer.Model;
+        if (model is OneVersusAllModelParameters oneVersusAllModelParameters)
+        {
+            model = oneVersusAllModelParameters.ToPublic();
+        }
+
+        return model;
     }
 }
